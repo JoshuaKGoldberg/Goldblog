4aa08f03c5238a3012453862ea2e72c3444cb25b
Josh Goldberg <josh@fullscreenmario.com>
2019-10-12 08:44:55 -0700

    Added GitHub link to footer

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -7,6 +7,10 @@ const Bio = () => {
         <div style={styles.footer}>
             <strong style={styles.strong}>Josh Goldberg</strong>
             Hi! I'm a frontend developer from New York. This is my blog about JavaScript, TypeScript, and scaling web application development.
+            <br />
+            This site's <a href="https://github.com/JoshuaKGoldberg/goldblog" rel="noopener noreferrer" target="_blank">open source on GitHub</a>.
+            {' '}
+            Found a problem? File an issue!
         </div>
     );
 };
