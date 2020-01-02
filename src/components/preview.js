bdc353a14ba379e7006396136fc334153adb40fe
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-01-02 11:52:27 -0500

    Allowed a blog title to be an external link (#84)
    * Allowed a blog title to be an external link
    
    * Fixed up imports

---
+++ src/components/preview.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from "gatsby";
+
+const ArticlePreview = ({ node }) => {
+    const title = node.frontmatter.title || node.fields.slug;
+
+    const link = node.link ? (
+        <a href={node.link} target="_blank">
+            {title}
+        </a>
+    ) : (
+        <Link to={node.fields.slug}>{title}</Link>
+    );
+
+    return (
+        <article>
+            <header>
+                <h2>{link}}</h2>
+                <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+                <p>{node.frontmatter.description || node.excerpt}</p>
+            </section>
+        </article>
+    );
+};
+
+export default ArticlePreview;
