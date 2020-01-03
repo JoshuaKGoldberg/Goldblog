6e7aa2a1e9407cbf858dff013d655ef5c5e23e45
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-01-03 12:29:08 -0500

    Added Google Analytics tracking ID (#97)

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
-                //trackingId: `ADD YOUR TRACKING ID HERE`,
+                trackingId: `UA-155298880-1`,
             },
         },
         `gatsby-plugin-feed`,
