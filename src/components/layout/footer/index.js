04c8f3cb601d876e974f1c34c88002893e330afe
Josh Goldberg <git@joshuakgoldberg.com>
2022-11-08 11:48:48 -0500

    Lil footer fix

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -6,8 +6,10 @@ const Bio = () => {
     return (
         <footer css={styles.footer}>
             <strong css={styles.strong}>Josh Goldberg</strong>
-            Hi! I'm a frontend developer from New York. This is my blog about
-            JavaScript, TypeScript, and open source web development.
+            Hi, I'm Josh! I'm a full time independent open source developer. I
+            work on projects in the TypeScript ecosystem such as
+            typescript-eslint and TypeStat. This is my blog about JavaScript,
+            TypeScript, and open source web development.
             <br />
             This site's{" "}
             <a
