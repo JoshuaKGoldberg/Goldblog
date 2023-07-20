8c6b0d9074a221b649045224c41a5028d660a0b4
Paul Esch-Laurent <herppfel@gmail.com>
2023-07-20 11:21:28 -0500

    fix: RSS feed (#392)
    * chore: add .nvmrc
    
    * docs: specify Yarn 1.x
    
    * chore: add gatsby-plugin-feed
    
    * feat: configure gatsby-plugin-feed to build feed
    
    * chore: add script to build && serve

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,5 +76,59 @@ module.exports = {
         `gatsby-plugin-emotion`,
         `gatsby-plugin-offline`,
         `gatsby-plugin-react-helmet`,
-    ],
-};
+        // https://www.gatsbyjs.com/plugins/gatsby-plugin-feed/#how-to-use
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+                {
+                    site {
+                        siteMetadata {
+                            title
+                            description
+                            siteUrl
+                            site_url: siteUrl
+                        }
+                    }
+                }
+                `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.nodes.map(node => {
+                                return Object.assign({}, node.frontmatter, {
+                                    description: node.excerpt,
+                                    date: node.frontmatter.date,
+                                    url: site.siteMetadata.siteUrl + node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + node.fields.slug,
+                                    custom_elements: [{ "content:encoded": node.html }],
+                                })
+                            })
+                        },
+                        query: `
+                        {
+                            allMarkdownRemark(
+                                sort: { order: DESC, fields: [frontmatter___date] },
+                                ) {
+                                    nodes {
+                                        excerpt
+                                        html
+                                        fields {
+                                            slug
+                                        }
+                                        frontmatter {
+                                            title
+                                            date
+                                        }
+                                    }
+                                }
+                            }
+                            `,
+                            output: "/rss.xml",
+                            title: "The RSS feed for Josh Goldberg's personal blog.",
+                        },
+                    ],
+                },
+            },
+        ],
+    };
